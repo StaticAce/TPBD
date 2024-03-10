3b8f7ebf2d3d8113727f277771a081d70e0322c4
Walter <bohnsackalexandruwalter@gmail.com>
2024-03-10 22:19:57 +0200

    Add delete and add functionality

--- ProiectTPBD/Form1.cs
+++ ProiectTPBD/Form1.cs
@@ -88,7 +88,9 @@ namespace ProiectTPBD
             DataGridView dataGridView = new DataGridView();
             TextBox textBox = new TextBox();
             Button buttonSearch = new Button();
+            Button buttonDelete = new Button(); // New delete button
             Label label = new Label();
+            Label labelMessage = new Label(); // New label for messages
 
             // DataGridView
             dataGridView.Anchor = AnchorStyles.Left | AnchorStyles.Top;
@@ -147,15 +149,66 @@ namespace ProiectTPBD
                 }
             };
 
+            dataGridView.CellBeginEdit += (cellSender, cellEventArgs) =>
+            {
+                if (dataGridView.Columns[cellEventArgs.ColumnIndex].Name != "Nume")
+                {
+                    cellEventArgs.Cancel = true;
+                }
+            };
+
             var db = new ProiectDbContext();
             dataGridView.DataSource = db.Angajati.ToList();
+
+            // buttonDelete
+            buttonDelete.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            buttonDelete.Location = new Point(panel1.Width - buttonDelete.Width - 105, 200); // Adjust the location as needed
+            buttonDelete.Size = new Size(150, 50);
+            buttonDelete.Text = "Stergere";
+            buttonDelete.Click += (deleteSender, deleteEventArgs) =>
+            {
+                if (dataGridView.SelectedRows.Count > 0)
+                {
+                    var confirmResult = new CustomMessageBox("Vreti sa stergeti angajatul selectat?", "Actiunea necesita confirmare").ShowDialog();
+                    if (confirmResult == DialogResult.Yes)
+                    {
+                        var db = new ProiectDbContext();
+                        var angajat = (Angajat)dataGridView.SelectedRows[0].DataBoundItem;
+                        db.Angajati.Remove(angajat);
+                        db.SaveChanges();
+                        dataGridView.DataSource = db.Angajati.ToList();
+                    }
+                }
+                else
+                {
+                    labelMessage.Text = "Nu ati selectat nici o persoana";
+                    labelMessage.Visible = true;
+                    System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer() { Interval = 3000 };
+                    timer.Tick += (timerSender, timerEventArgs) =>
+                    {
+                        labelMessage.Visible = false;
+                        timer.Stop();
+                    };
+                    timer.Start();
+                }
+            };
+
+            // labelMessage
+            labelMessage.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            labelMessage.Location = new Point(panel1.Width - labelMessage.Width - 270, 100); // Adjust the location as needed
+            labelMessage.AutoSize = true;
+            labelMessage.Visible = false;
+            labelMessage.ForeColor = Color.Red;
 
             panel1.Controls.Clear();
             panel1.Controls.Add(dataGridView);
             panel1.Controls.Add(textBox);
             panel1.Controls.Add(buttonSearch);
+            panel1.Controls.Add(buttonDelete); // Add the delete button to the panel
             panel1.Controls.Add(label);
+            panel1.Controls.Add(labelMessage); // Add the message label to the panel
         }
+
 
         private void AdaugareAngajatiToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -224,11 +277,85 @@ namespace ProiectTPBD
             var db = new ProiectDbContext();
             dataGridView.DataSource = db.Angajati.ToList();
 
+            // Label for Nume
+            Label labelNume = new Label();
+            labelNume.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            labelNume.Location = new Point(panel1.Width - labelNume.Width - 140, 300);
+            labelNume.Size = new Size(70, 50);
+            labelNume.Text = ("Nume:");
+
+            // TextBox for nume
+            TextBox textBoxNume = new TextBox();
+            textBoxNume.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            textBoxNume.Location = new Point(panel1.Width - textBoxNume.Width - 80, 300);
+            textBoxNume.Size = new Size(150, 50);
+
+            // Label for Salar
+            Label labelSalar = new Label();
+            labelSalar.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            labelSalar.Location = new Point(panel1.Width - labelSalar.Width - 140, 350);
+            labelSalar.Size = new Size(70, 50);
+            labelSalar.Text = ("Salar:");
+
+            // TextBox for salar
+            TextBox textBoxSalar = new TextBox();
+            textBoxSalar.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            textBoxSalar.Location = new Point(panel1.Width - textBoxSalar.Width - 80, 350);
+            textBoxSalar.Size = new Size(150, 50);
+
+            // Label for error message
+            Label labelError = new Label();
+            labelError.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            labelError.Location = new Point(panel1.Width - labelError.Width - 290, 450);
+            labelError.Size = new Size(200, 50);
+            labelError.ForeColor = Color.Red;
+
+            // Button for adding new object
+            Button buttonAdd = new Button();
+            buttonAdd.Anchor = AnchorStyles.Right | AnchorStyles.Top;
+            buttonAdd.Location = new Point(panel1.Width - buttonAdd.Width - 105, 450);
+            buttonAdd.Size = new Size(150, 50);
+            buttonAdd.Text = "Adaugare";
+
+            buttonAdd.Click += (addSender, addEventArgs) =>
+            {
+                string nume = textBoxNume.Text.Trim();
+                string salarStr = textBoxSalar.Text.Trim();
+                int salar;
+
+                if (string.IsNullOrEmpty(nume) || !int.TryParse(salarStr, out salar))
+                {
+                    labelError.Text = "Datele introduse nu sunt permise!";
+                    labelError.Visible = true;
+                    System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer() { Interval = 3000 };
+                    timer.Tick += (timerSender, timerEventArgs) =>
+                    {
+                        labelError.Visible = false;
+                        timer.Stop();
+                    };
+                    timer.Start();
+                }
+                else
+                {
+                    var db = new ProiectDbContext();
+                    var angajat = new Angajat { Nume = nume, Salar = salar };
+                    db.Angajati.Add(angajat);
+                    db.SaveChanges();
+
+                    dataGridView.DataSource = db.Angajati.ToList();
+                }
+            };
             panel1.Controls.Clear();
             panel1.Controls.Add(dataGridView);
             panel1.Controls.Add(textBox);
             panel1.Controls.Add(buttonSearch);
             panel1.Controls.Add(label);
+            panel1.Controls.Add(textBoxNume);
+            panel1.Controls.Add(textBoxSalar);
+            panel1.Controls.Add(labelError);
+            panel1.Controls.Add(buttonAdd);
+            panel1.Controls.Add(labelNume);
+            panel1.Controls.Add(labelSalar);
         }
 
         private void ActualizareDateToolStripMenuItem_Click(object sender, EventArgs e)
